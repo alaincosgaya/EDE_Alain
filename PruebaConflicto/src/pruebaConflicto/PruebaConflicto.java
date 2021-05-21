221d34826ee270a9ede924d70deeddc2e2b9b3cd
1dam <1dam@HZ301190.TartangaLH.eus>
2021-05-21 12:09:04 +0200

    Commit - 1

---
+++ PruebaConflicto/src/pruebaConflicto/PruebaConflicto.java
@@ -0,0 +1,10 @@
+package pruebaConflicto;
+
+public class PruebaConflicto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+
+}
