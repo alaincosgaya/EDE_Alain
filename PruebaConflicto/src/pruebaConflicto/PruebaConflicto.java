44c0b9e6e454bfb14c75490400e23963ca302130
1dam <1dam@HZ301190.TartangaLH.eus>
2021-05-21 12:33:35 +0200

    commit sin conflicto

--- PruebaConflicto/src/pruebaConflicto/PruebaConflicto.java
+++ PruebaConflicto/src/pruebaConflicto/PruebaConflicto.java
@@ -5,6 +5,7 @@ public class PruebaConflicto {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo-desde Eclipse2 ");
+		System.out.println("Hola mundo desde GitHub");
 	}
 
 }
